cdda3097bd01794c28f046775a2226feadfc876f
Marius <marius_ogrean@yahoo.com>
2026-03-28 11:13:12 +0200

    add spring boot project

---
+++ src/test/java/com/testCrawler/api/ApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.testCrawler.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
