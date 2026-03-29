ea60ca6535971f7bb1d589d62222af79a27efdf1
Marius <marius_ogrean@yahoo.com>
2026-03-29 15:31:32 +0300

    make method post

--- src/main/java/com/testCrawler/api/controllers/CompanyDataController.java
+++ src/main/java/com/testCrawler/api/controllers/CompanyDataController.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +46,7 @@ public class CompanyDataController {
         return companyDataService.getFillRate();
     }
 
-    @GetMapping(value = "/getMatch")
+    @PostMapping(value = "/getMatch")
     public MatchOutput getMatch(@RequestBody Input input) {
         return companyDataService.matchInput(input);
     }
